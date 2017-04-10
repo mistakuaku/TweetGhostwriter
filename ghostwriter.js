171baa1c27796ad2a26fa38434cfd56af9462b32
mistakuaku <dragonz139@hotmail.com>
2017-04-10 12:15:02 -0700

    Made a meme generator

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,34 @@
 function writeTweet()
 {
-  
+  var randIndex, sentence;
+
+  var intro = ["What is it with ",
+              "Have you ever noticed ",
+              "Does anyone remember ",
+              "What is wrong with "];
+
+  var body = ["Taco Bell drive-thru",
+              "Battletoads",
+              "Jiffylube",
+              "Jared from Subway",
+              "Reddit"];
+  var end = [" you never get what you want",
+            "?",
+            " it's always full of stale memes"];
+
+  randIndex = randomUpTo(intro.length - 1);
+
+  sentence = intro[randIndex];
+
+  randIndex = randomUpTo(body.length - 1);
+
+  sentence = sentence + " " + body[randIndex];
+
+  randIndex = randomUpTo(end.length - 1);
+
+  sentence = sentence + " " + end[randIndex];
+
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
